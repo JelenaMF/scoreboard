e6810ffde4f5347dec815b4e8aa2f01d05a0a295
Jelena Feliciano <jfel217@gmail.com>
2021-05-04 17:40:22 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-const desc = 'I just learned how to create a React node and render it into the DOM.'
-const myTitleId = 'main-title'
-const Name = "Jelena"
-
-const header = (
-    <header>
-        <h1 id={myTitleId}> {Name}'s First React Element </h1>
-        <p> {desc} </p>
-    </header>
-    
-)
+function Header() {
+    return (
+        <header>
+            <h1>Scoreboard</h1>
+            <span className="stats">Players: 1</span>
+        </header>
+    );
+}
 
 ReactDOM.render(
-    header, 
+    <Header/>, 
     document.getElementById('root')
 );
