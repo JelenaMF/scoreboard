a05f7638d29a0176ee012dd15e1e9be273cf80c8
Jelena Feliciano <jfel217@gmail.com>
2021-05-04 23:16:45 -0400

    added a Counter and App component

--- app.js
+++ app.js
@@ -6,8 +6,49 @@ const Header = () => {
         </header>
     );
 }
+/**
+ * 
+ * @returns players name and a counter that displays the players score 
+ *  
+ */
+const Player = () => {
+    return (
+        <div className="player">
+            <span className="player-name">
+                Lenny 
+                 </span>
+            <Counter />
+        </div>
+    );
+}
+
+/**
+ * 
+ * @returns a counter with buttons that allow user to change the score + || -
+ *  
+ */
+
+const Counter = () => {
+    return (
+        <div className="counter">
+            <button className="counter-action decrement"> - </button>
+            <span className="counter-score">35</span>
+            <button className="counter-action increment"> + </button>
+        </div>
+    )
+}
+
+const App = ( ) => {
+    return (
+        <div className="scoreboard">
+            <Header />
+            {/** Players List  */}
+            <Player />
+        </div>
+    );
+}
 
 ReactDOM.render(
-    <Header />, 
+    <App />, 
     document.getElementById('root')
 );
